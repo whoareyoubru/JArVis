6c7c30fdb0970d56fd727c17b75879e28e81a8c4
Hackedlak <68181994+Lakhac@users.noreply.github.com>
2020-08-07 10:35:12 +0530

    Add files via upload

---
+++ ins.sh
@@ -0,0 +1,29 @@
+#!data/data/com.termux/files/etc
+#Tool By Noob Hackers 
+#ThAnKS For Supporting 
+################
+cyn='\e[0;36m'
+
+lgrn='\e[1;32m'
+
+r='\e[1;31m'
+
+y='\e[1;33m'
+################
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\nWrite Your \e[31mFirst \e[33mName\e[32m 👇\n\n"
+read varbanner
+ech
+echo "toilet -f mono12 -F gay -F border '$varbanner' |lolcat -a -d 1 -t -s 100" > banner.txt
+echo 
+echo "clear" > null.txt
+cat "null.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "banner.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "tload.txt"  >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+echo " "
+echo "                   "Ready to go buddy..... | lolcat
+echo " "
---
+++ thanks.sh
@@ -0,0 +1,32 @@
+cyn='\e[0;36m'
+
+lgrn='\e[1;32m'
+
+r='\e[1;31m'
+
+y='\e[1;33m'
+################
+echo ' 
+ 
+       
+ __ __    ___  _      _       ___  
+|  |  |  /  _]| |    | |     /   \ 
+|  |  | /  [_ | |    | |    |     |
+|  _  ||    _]| |___ | |___ |  O  |
+|  |  ||   [_ |     ||     ||     |
+|  |  ||     ||     ||     ||     |
+|__|__||_____||_____||_____| \___/ 
+                                   
+' | lolcat
+echo " "
+echo " "
+cat letter.txt
+echo " "
+echo "                      " thanks for using please subscrib our channel| lolcat
+echo " " 
+echo " "
+sleep 16.0
+clear 
+exit 0                
+sub my YouTube channel 
+                      
